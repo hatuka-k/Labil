975b87b46f1460b2632a8bf9adfde8e97fb53bf7
Erisa <erisa.matsumoto.7u.stu.hosei.ac.jp>
2016-07-07 14:41:41 +0900

    auto update, improvement of popover

--- app/controllers/topviews_controller.rb
+++ app/controllers/topviews_controller.rb
@@ -8,8 +8,10 @@ class TopviewsController < ApplicationController
     @stats = Array.new
     @statuses.each do |status|
       @stat = Hash.new
+      @stat[:id] = status.id
       @stat[:member] = @members.find(status.member_id).name
       @stat[:location] = @locations.find(status.location_id).name
+      @stat[:location_category] = @locations.find(status.location_id).category
       if defined?( @messages.find_by(member_id: status.member_id).message)
         @stat[:message] = @messages.find_by(member_id: status.member_id).message
       else
