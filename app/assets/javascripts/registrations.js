928a128e8938f6ba7e1e9bac34a18ffd9dc67146
yubeshi <fuko.takano.2c@stu.hosei.ac.jp>
2016-07-07 08:42:51 -0400

    registration function is added

--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -1,4 +1,14 @@
-function move2regist(usid, path) {
-	link = "regist";
-	href = path;
+function kakunin(btnNo){
+  if (btnNo == 1){
+    link = "Yahoo!Japan";
+    href = "http://www.yahoo.co.jp/";
+  }else{
+    link = "Google";
+    href = "http://www.google.co.jp/";
+  }
+
+  ret = confirm(link + "へ飛びます。宜しいですか？");
+  if (ret == true){
+    location.href = href;
+  }
 }
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,46 @@
 class RegistrationsController < ApplicationController
   def index
-	  @locations = Location.where(category: 0)
+	  @locations = Location.all
 	  @stat = Status.find(params[:id])
 	  @members = Member.all
-	  @user = Status.find(1)
 	  @name = Member.find(@stat.member_id)
-	  @user.update_attribute(:location_id, 0)
+	  @nloc = Location.find(@stat.location_id)
+	  @zaishitsu = "/registrations/zaishitsu/"+params[:id]
+	  @gakunai = "/registrations/gakunai/"+params[:id]
+	  @gaishutsu = "/registrations/gaishutsu/"+params[:id]
+	  @kitaku = "/registrations/kitaku/"+params[:id]
   end
   
+  #put registrations/zaishitsu  
+  def zaishitsu
+    @stat = Status.find(params[:id])
+    @stat.location_id = 1;
+    if @stat.save
+      redirect_to topviews_index_path
+    end
+  end
+  def gakunai
+    @classrooms = Location.where("category = '1'")
+    @labos = Location.where("category = '2'")
+    @stat = Status.find(params[:id])
+    @stat.location_id = 1;
+    @stat.save
+  end
+  def gaishutsu
+    @stat = Status.find(params[:id])
+    @stat.location_id = 1;
+    if @stat.save
+      redirect_to topviews_index_path
+    end
+  end
+  def kitaku
+    @stat = Status.find(params[:id])
+    @loc = Location.where("category = '4'")
+    @stat.location_id = @loc.id;
+    if @stat.save
+      redirect_to topviews_index_path
+    end
+  end
 
   def new
     @locations = Location.all
